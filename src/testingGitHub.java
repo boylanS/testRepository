f9d07326ea2a10e11aaf99df38576c5d92df6bf9
boylanS <sadbhboylan@gmail.com>
2022-02-18 21:16:27 +0000

    Second Line

--- src/testingGitHub.java
+++ src/testingGitHub.java
@@ -3,5 +3,7 @@ public class testingGitHub {
     public static void main(String[] args){
 
         System.out.println("Hello World :)");
+
+        System.out.println("Adding a second line to test commits");
     }
 }
